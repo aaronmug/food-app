0d7f02ef06a51812c7d3401f6ec28a494bc5566c
Aaron Mugume <aaronmug091@gmail.com>
2024-09-03 12:04:58 +0300

    Dynamic Screen Options Commit

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,9 +1,10 @@
+import { useLayoutEffect } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   // route provided by Stack.Screen
   //   const route = useRoute(); for components not registered using Stack.Screen
   const catId = route.params.categoryId;
@@ -11,6 +12,17 @@ function MealsOverviewScreen({ route }) {
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0; // that implies the category exists
   });
+
+  useLayoutEffect(() => {
+    // run effect simultaneously with component function execution
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]); // two external dependencies
 
   function renderMealItem(itemData) {
     const item = itemData.item;
